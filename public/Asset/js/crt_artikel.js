f1dd47b4e547629af3e11a3b79a6c7d56fee81c1
LuxTenebris99 <gusti.furkanazmi@student.untan.ac.id>
2022-08-09 19:12:15 +0700

    preview img artikel

---
+++ public/Asset/js/crt_artikel.js
@@ -0,0 +1,39 @@
+function thumbnail(id, nama) {
+  $("#urlModal").modal("show");
+}
+
+var tb_list;
+$(document).ready(function () {
+  tb_list = $("#tbfoto").DataTable({
+    ajax: "/AjaxBlog/getFotoAll",
+    select: true,
+    columns: [
+      { data: "id" },
+      { data: "url" },
+      // { data: "url" },
+      { data: "alt" },
+    ],
+    columnDefs: [
+      {
+        targets: 1,
+        render: function (data) {
+          return '<img src="' + data + '" class="img-fluid img-thumbnail">';
+        },
+      },
+    ],
+  });
+
+  $("#tbfoto tbody").on("click", "td", function () {
+    var data = tb_list.row($(this).parents("tr")).data();
+
+    console.log(data);
+    $("#inputThumbnail").val(data.url);
+    $(".card-img-top").attr("src", data.url);
+    $("#urlModal").modal("hide");
+  });
+
+  $("#inputThumbnail").on("change", function () {
+    console.log(this.value);
+    $(".card-img-top").attr("src", this.value);
+  });
+});
